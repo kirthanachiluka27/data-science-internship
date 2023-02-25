f15c086743f783c0179003b70d790fc4803ac898
kirthanachiluka27 <109787533+kirthanachiluka27@users.noreply.github.com>
2023-02-25 16:09:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.header("Hello World")
+st.write('DataScience internship 2023.')
+st.title(":red[Innomatics] Data App :sunglasses")
+CODE= """print("hello world")"""
+st.code(CODE,language="python")
+
